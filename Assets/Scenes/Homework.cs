4280a9a52a2642b9d447b771f03a6db22d72ee78
fumihiko hata <switchingprotocol@outlook.com>
2020-07-26 07:10:59 +0900

    Add files.

---
+++ Assets/Scenes/Homework.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss {
+    private int hp = 100;
+    private int mp = 53;
+    private int power = 25;
+    const int MAGIC_CONSUMPTION = 5;
+
+    public void Attack() {
+        Debug.Log(this.power + " のダメージを与えた");
+    }
+
+    public void Magic() {
+        if (this.mp >= MAGIC_CONSUMPTION) {
+            this.mp -= MAGIC_CONSUMPTION;
+            Debug.Log("魔法攻撃をした。残りMPは " + this.mp);
+        } else {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+
+    public void Defence(int damage) {
+        Debug.Log(damage + " のダメージを受けた");
+        this.hp -= damage;
+    }
+}
+
+public class Homework : MonoBehaviour {
+
+    private void ShowSparator(string s) {
+        Debug.Log(new string('-', 10) + " " + s + " " + new string('-', 10));
+    }
+
+    void Start() {
+
+        // Homework
+        ShowSparator("Homework");
+        int[] a = {10, 20, 30, 40, 50};
+        foreach (var item in a) {
+            Debug.Log(item);
+        }
+        int[] b = {10, 20, 30, 40, 50};
+        for (int i = b.Length - 1; i >= 0 ; i--) {
+            Debug.Log(b[i]);
+        }
+
+        // Advanced homework
+        ShowSparator("Advanced Homework");
+        Boss lastboss = new Boss();
+        lastboss.Attack();
+        lastboss.Defence(3);
+        for (int i = 0; i < 10; i++) {
+            lastboss.Magic();
+        }
+        Debug.Log("---- 次の魔法攻撃はMP不足で失敗すると思われる。");
+        lastboss.Magic();
+    }
+
+    void Update() {/*nothing*/}
+}
---
+++ Assets/Scenes/Test.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    void Hello() {
+        Debug.Log("Hello, UnityChan👏");
+    }
+
+    int Add(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+        // Function
+        Hello();
+        int num = Add(3, 6);
+        Debug.Log(num);
+    }
+
+    void Start2() {
+
+        // Hello World
+        Debug.Log("Hello, World!!");
+
+        // Valiables
+        int score;
+        score = 150;
+        Debug.Log(score);
+
+        // Calculation
+        int a = 3;
+        int b = 4;
+        int c = a + b;
+        Debug.Log(c);
+
+        // Condition
+        int money = 100;
+        if (money <= 50) {
+            Debug.Log("武器を売る");
+        } else if (money >= 200) {
+            Debug.Log("武器を買う");
+        } else {
+            Debug.Log("ポーションを買う");
+        }
+
+        // Ternary operator
+        int num = 1;
+        // int val = (num == 1) ? -100 : 100; // 下と等価。
+        int val = num == 1 ? -100 : 100;
+        Debug.Log(val);
+
+        // Scope
+        int aa = 3;
+        if (aa == 3) {
+            int bb = 5;
+            Debug.Log(bb); // OK
+        }
+        // Debug.Log(b); // error
+
+        // Loop
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        Debug.Log("合計は " + sum);
+
+        // Array
+        int[] points = new int[5];
+        points[0] = 30;
+        points[1] = 20;
+        points[2] = 50;
+        points[3] = 10;
+        points[4] = 80;
+        for (int i = 0; i < 5; i++) {
+            Debug.Log(points[i]);
+        }
+        int[] points2 = {30, 20, 50, 10, 80, 15, 60, 100};
+        for (int i = 0; i < points2.Length; i++) {
+            if (points2[i] >= 50) {
+                Debug.Log(points2[i]);
+            }
+        }
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
